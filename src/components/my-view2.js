deef0f953440491f0a3fae4a21c03c05489596fa
Artem <gusev2014russia@mail.ru>
2019-07-10 21:23:47 +0300

    add some changes

--- src/components/my-view2.js
+++ src/components/my-view2.js
@@ -11,26 +11,16 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 import { html } from 'lit-element';
 import { PageViewElement } from './page-view-element.js';
 import { connect } from 'pwa-helpers/connect-mixin.js';
+import { SharedStyles } from './shared-styles.js';
 
 // This element is connected to the Redux store.
 import { store } from '../store.js';
-
-// These are the actions needed by this element.
-import { increment, decrement } from '../actions/counter.js';
-
-// We are lazy loading its reducer.
-import counter from '../reducers/counter.js';
 store.addReducers({
   counter
 });
 
-// These are the elements needed by this element.
-import './counter-element.js';
 
-// These are the shared styles needed by this element.
-import { SharedStyles } from './shared-styles.js';
-
-class MyView2 extends connect(store)(PageViewElement) {
+class ToDoDescription extends connect(store)(PageViewElement) {
   static get properties() {
     return {
       // This is the data from the store.
--- src/store.js
+++ src/store.js
@@ -19,15 +19,8 @@ import { lazyReducerEnhancer } from 'pwa-helpers/lazy-reducer-enhancer.js';
 
 import app from './reducers/app.js';
 
-// Sets up a Chrome extension for time travel debugging.
-// See https://github.com/zalmoxisus/redux-devtools-extension for more information.
 const devCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// Initializes the Redux store with a lazyReducerEnhancer (so that you can
-// lazily add reducers after the store has been created) and redux-thunk (so
-// that you can dispatch async actions). See the "Redux and state management"
-// section of the wiki for more details:
-// https://github.com/Polymer/pwa-starter-kit/wiki/4.-Redux-and-state-management
 export const store = createStore(
   state => state,
   devCompose(
@@ -35,7 +28,6 @@ export const store = createStore(
     applyMiddleware(thunk))
 );
 
-// Initially loaded reducers.
 store.addReducers({
   app
 });
